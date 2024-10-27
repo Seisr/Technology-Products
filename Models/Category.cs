42d0e49bb0825c539026003c6496457f616f4fdc
Bam <khanguyen1204@gmail.com>
2024-10-27 18:26:05 +0700

    add feat

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebApp.Models;
 public class Category
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public byte CategoryId { get; set; }
     public string CategoryName { get; set; } = null!; //not null
 
